9a6ae910d216d9d02b288b7de893b5e38524e076
MinKi1506 <93629619+MinKi1506@users.noreply.github.com>
2022-08-01 17:53:50 +0900

    store info(20%)

--- practice/src/components/StoreInfo.js
+++ practice/src/components/StoreInfo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../styles/storeInfo.css';
 import { useNavigate } from 'react-router-dom';
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+
 
 
 function StoreInfo() {
@@ -57,9 +60,8 @@ function StoreInfo() {
                 columnGap: '1px',
                 gridTemplateRows: 'repeat(4, 1fr)',
                 rowGap: '1px',
-                height: '200px',
+                height: '180px',
                 width: '100%',
-                border: 'solid 1px',
                 padding: '5px'
             }}>
                 <div style={{
@@ -71,31 +73,90 @@ function StoreInfo() {
                 </div>
                 <div style={{
                     border: 'solid 1px',
-                  
+
                 }}>
                     <img className='store_photo' src='./img/store_3.png'></img>
                 </div>
                 <div style={{
                     border: 'solid 1px',
-                   
+
                 }}>
                     <img className='store_photo' src='./img/store_4.png'></img>
                 </div>
                 <div style={{
                     border: 'solid 1px',
-                  
+
                 }}>
                     <img className='store_photo' src='./img/store_5.png'></img>
                 </div>
                 <div style={{
                     border: 'solid 1px',
                     padding: '20px',
-                 
+
+
                 }}>
                     <img className='store_photo' src='./img/more.png'></img>
                 </div>
             </div>
-            <div>
+            <div style={{
+                textAlign: 'center',
+                fontFamily: 'cafe',
+                fontSize: '30px',
+                borderBottom: 'double 5px'
+            }}>
+                <h3 style={{
+                    display: 'inline-block'
+                }}>업체명 <span style={{
+                    fontSize: '15px'
+                }}>＜점포 카테고리＞</span></h3>
+            </div>
+            <div style={{
+                padding: '10px',
+                borderBottom: 'double 5px'
+            }}>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    marginTop: '5px',
+                    marginBottom: '10px'
+                }}>
+                    <h3><span style={{
+                        color: 'blue',
+                        fontWeight: 'bold',
+
+                    }}>n</span>개의 리뷰가 있는 곳이에요!</h3>
+
+                    <AwesomeButton style={{
+                        width: '100px'
+                    }} type='secondary'>리뷰 보러 가기</AwesomeButton>
+
+                </div>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}>
+                    <h3>검증중인 리뷰가 <span style={{
+                        color: 'red',
+                        fontWeight: 'bold'
+                    }}>n</span>개있어요.</h3>
+
+                    <AwesomeButton style={{
+                        width: '100px',
+                        marginBottom: '10px'
+                    }} type='secondary'>투표 하러 가기</AwesomeButton>
+                </div>
+            </div>
+            <div style={{
+                boderBottom: 'borderBottom: double 5px'
+            }}>
+                <div>
+                    <h3>주소: <span>경남 거제시 일운면 지세포로 122</span></h3>
+                    <h3>영업시간: <span>오전 11시</span>~<span>오후 10시</span></h3>
+                    <h3>홈페이지: <span>www.naver.com</span></h3>
+                    <h3>Instagram: <span>www.naver.com</span></h3>
+                    <h3>Tel: <span>055-541-8426</span></h3>
+                </div>
+
             </div>
         </div>
     );
