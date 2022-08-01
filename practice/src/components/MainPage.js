b38ef8bde20102990ae35d9d09ab00ff726c3005
MinKi1506 <93629619+MinKi1506@users.noreply.github.com>
2022-08-01 13:29:25 +0900

    8.1 project

--- practice/src/components/MainPage.js
+++ practice/src/components/MainPage.js
@@ -8,222 +8,273 @@ import { useState } from 'react';
 import { Routes, Route, Link, useNavigate, BrowserRouter as Router } from 'react-router-dom';
 
 
-function MainPage(){
+function MainPage() {
 
-    
   let [review, setReview] = useState();
   let navigate = useNavigate();
+  let  btc_price, btc_change, eth_price, eth_change, xrp_price, xrp_change, neo_price, neo_change = 0;
+  const [coin, setCoin] = useState({});
 
-  //코인 시세 업데이트 기능
-  let [quoation, setquoation] = useState();
 
-//   TODO
-  let updateQuoation = () => {
-    axios({
-      method: 'get',
-      url: ''
-    }
-      .then(function (respons) {
-        console.log(respons[0][0]);
-        console.log(respons[0][1]);
-        console.log(respons[0][2]);
-        console.log(respons[0][3]);
-        console.log(respons[0][4]);
-        console.log(respons[0][5]);
-        console.log(respons[0][6]);
-        console.log(respons[0][7]);
-
-        setquoation(respons[0]);
-        //궁금한 것: db에서는 10초마다 갱신이 되는데, 이 때 useEffect를 써도 갱신 될 때 마다 함수가 재실행 되나요?
-        //안그럴 것 같은데, 이럴 때는 여기도 10초마다 갱신하는 함수를 짜야하죠? 
-      }))
+  // 코인 시세 업데이트 기능
+  // cors 이슈 참고: https://sennieworld.tistory.com/m/49 
+  function updateQuoation(){
+    axios.get('https://api.upbit.com/v1/ticker',{
+      params: {
+        markets: 'KRW-BTC'
+      }
+    })
+    .then(function(response){
+      console.log(response.data[0].trade_price);
+      console.log(response.data[0].change_price);
+      btc_price = response.data[0].trade_price;
+      btc_change = response.data[0].change_price;
+    })
+    .catch(function(error){
+      console.log('에러: '+error);
+    });
+    axios.get('https://api.upbit.com/v1/ticker',{
+      params: {
+        markets: 'KRW-ETH'
+      }
+    })
+    .then(function(response){
+      console.log(response.data[0].trade_price);
+      console.log(response.data[0].change_price);
+      eth_price = response.data[0].trade_price;
+      eth_change = response.data[0].change_price;
+    })
+    .catch(function(error){
+      console.log('에러: '+error);
+    });
+    axios.get('https://api.upbit.com/v1/ticker',{
+      params: {
+        markets: 'KRW-XRP'
+      }
+    })
+    .then(function(response){
+      console.log(response.data[0].trade_price);
+      console.log(response.data[0].change_price);
+      xrp_price = response.data[0].trade_price;
+      xrp_change = response.data[0].change_price;
+    })
+    .catch(function(error){
+      console.log('에러: '+error);
+    });
+    axios.get('https://api.upbit.com/v1/ticker',{
+      params: {
+        markets: 'KRW-NEO'
+      }
+    })
+    .then(function(response){
+      console.log(response.data[0].trade_price);
+      console.log(response.data[0].change_price);
+      neo_price = response.data[0].trade_price;
+      neo_change = response.data[0].change_price;
+    })
+    .catch(function(error){
+      console.log('에러: '+error);
+    });
   }
 
-    return (
-        <div>
-            <div className="App box" style={{
-      border: 'solid 1px',
-      width: '360px',
-      height: '640px',
-      overflow: 'auto',
-      scrollbarWidth: 'none',
-      fontFamily: 'nanum'
-    }}>
 
-      <div style={{
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'space-between',
-        alignItems: 'center',
+
+  updateQuoation();
+ setInterval(updateQuoation,10000);
+ 
+  return (
+    <div>
+      <div className="App box" style={{
+        border: 'solid 1px',
+        width: '360px',
+        height: '640px',
+        overflow: 'auto',
+        scrollbarWidth: 'none',
+        fontFamily: 'nanum'
       }}>
 
-        {/* 캐릭터 정보  */}
-        <img onClick={() => { navigate('/myInfo') }} src='/img/profile_sample.png' style={{
-          width: '60px',
-          height: '60px',
-          borderRadius: '70%'
-        }}></img>
-
-        {/* 로고 */}
-        <div>
-          <h2 style={{
-            fontSize: '50px',
-            fontFamily: 'cute'
-          }}>Wanna</h2>
-        </div>
-
-        {/* 네비게이션바 */}
-        <button style={{
-          border: 'none',
-          backgroundColor: 'white'
-        }}><img style={{
-          width: '40px',
-          height: '40px'
-        }} src='/img/menu.png'></img>
-        </button>
-      </div>
-
-      <div style={{
-        height: '400px'
-      }}>
-        <div style={{
-          height: '20px'
-        }}>
-
-        </div>
-        <h5 style={{
-          fontFamily: 'cute'
-        }}>OOO님, 안녕하세요!</h5>
-
-        {/* 검색창 */}
         <div style={{
           display: 'flex',
-          justifyContent: 'flex-start',
-          alignItems: 'center'
+          width: '100%',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}>
-          <img style={{
-            width: '50px',
-            height: '50px'
-          }} src='/img/search.png'></img>
-          <div class="inner">
-            <input type="search" />
-            <div class="searching">
-            </div>
-            <div class="icon_ect">
+
+          {/* 캐릭터 정보  */}
+          <img onClick={() => { navigate('/myInfo') }} src='/img/profile_sample.png' style={{
+            width: '60px',
+            height: '60px',
+            borderRadius: '70%'
+          }}></img>
+
+          {/* 로고 */}
+          <div>
+            <h2 style={{
+              fontSize: '50px',
+              fontFamily: 'cute'
+            }}>Wanna</h2>
+          </div>
+
+          {/* 네비게이션바 */}
+          <button style={{
+            border: 'none',
+            backgroundColor: 'white'
+          }}><img style={{
+            width: '40px',
+            height: '40px'
+          }} src='/img/menu.png'></img>
+          </button>
+        </div>
+
+        <div style={{
+          height: '400px'
+        }}>
+          <div style={{
+            height: '20px'
+          }}>
+
+          </div>
+          <h5 style={{
+            fontFamily: 'cute'
+          }}>OOO님, 안녕하세요!</h5>
+
+          {/* 검색창 */}
+          <div style={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            alignItems: 'center'
+          }}>
+            <img style={{
+              width: '50px',
+              height: '50px'
+            }} src='/img/search.png'></img>
+            <div class="inner">
+              <input type="search" />
+              <div class="searching">
+              </div>
+              <div class="icon_ect">
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* 기능 버튼 */}
-        <div>
-          <Button style={{
-            height: '70px',
-            width: '70px'
-          }} variant="outline-primary">여행가기</Button>{' '}
-          <Button style={{
-            height: '70px',
-            width: '70px'
-          }} variant="outline-primary">우리 지역 <br></br> 큐레이팅</Button>{' '}
-          <Button style={{
-            height: '70px',
-            width: '70px'
-          }} variant="outline-primary">이벤트</Button>{' '}
-          <Button style={{
-            height: '70px',
-            width: '70px'
-          }} variant="outline-primary">더 보기</Button>{' '}
-        </div>
+          {/* 기능 버튼 */}
+          <div>
+            <Button style={{
+              height: '70px',
+              width: '70px'
+            }} variant="outline-primary">여행가기</Button>{' '}
+            <Button style={{
+              height: '70px',
+              width: '70px'
+            }} variant="outline-primary">우리 지역 <br></br> 큐레이팅</Button>{' '}
+            <Button style={{
+              height: '70px',
+              width: '70px'
+            }} variant="outline-primary">이벤트</Button>{' '}
+            <Button style={{
+              height: '70px',
+              width: '70px'
+            }} variant="outline-primary">더 보기</Button>{' '}
+          </div>
 
-        {/* 실시간 코인 시세 현황 */}
-        <div style={{
-          margin: '30px 10px 10px 10px'
-        }}>
-          <Table striped bordered hover size="sm" style={{
-            // borderRadius: '15px',
-            // borderCollapse: 'collapse',
+          {/* 실시간 코인 시세 현황 */}
+          <div style={{
+            margin: '30px 10px 10px 10px'
           }}>
-            <thead>
-              <tr>
-                <th>코인</th>
-                <th>현재 가격(원)</th>
-                <th>변화량(원)</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>BitCoin</td>
-                <td>Thornton</td>
-              </tr>
-              <tr>
-                <td>eth</td>
-                <td>Thornton</td>
-              </tr>
-              <tr>
-                <td>솔라나</td>
-                <td>Thornton</td>
-              </tr>
-            </tbody>
-          </Table>
-        </div>
+            <Table striped bordered hover size="sm" style={{
+              // borderRadius: '15px',
+              // borderCollapse: 'collapse',
+            }}>
+              <thead>
+                <tr>
+                  <th>코인</th>
+                  <th>현재 가격(원)</th>
+                  <th>변화량(원)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>BitCoin</td>
+                  <td>{btc_price}</td>
+                  <td>{btc_change}</td>
+                </tr>
+                <tr>
+                  <td>Ethereum</td>
+                  <td>{eth_price}</td>
+                  <td>{eth_change}</td>
+                </tr>
+                <tr>
+                  <td>XRP</td>
+                  <td><h2>{xrp_price}</h2></td>
+                  <td><h2>{xrp_change}</h2></td>
+                </tr>
+                <tr>
+                  <td>NEO</td>
+                  <td>{neo_price}</td>
+                  <td>{neo_change}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
 
-        {/* 베스트 리뷰 */}
-        <div style={{
-          padding: '10px'
-        }}>
-          <Carousel>
-            <Carousel.Item interval={1000}>
-              <img style={{
-                borderRadius: '15px'
-              }}
-                className="d-block w-100"
-                src="\img\pusan.jpg"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3 style={{
-                  fontSize: '20px'
-                }}>부산 해운대</h3>
-                <h6>해운대 앞바다와 마천루가 선사하는 근사한 야경</h6>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={500}>
-              <img style={{
-                borderRadius: '15px'
-              }}
-                className="d-block w-100"
-                src="\img\bookchon.jpg"
-                alt="Second slide"
-              />
-              <Carousel.Caption>
-                <h3 style={{
-                  fontSize: '20px'
-                }}>서울 북촌 한옥마을</h3>
-                <h6>고즈넉한 분위기의 전통 한옥마을</h6>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img style={{
-                borderRadius: '15px'
-              }}
-                className="d-block w-100"
-                src="\img\geoje.jpg"
-                alt="Third slide"
-              />
-              <Carousel.Caption>
-                <h3 style={{
-                  fontSize: '20px'
-                }}>경남 거제시</h3>
-                <h6>조용하고 따뜻한 바다마을을 여행하고 싶다면.. 거제로!</h6>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </div>
+          {/* 베스트 리뷰 */}
+          <div style={{
+            padding: '10px'
+          }}>
+            <Carousel>
+              <Carousel.Item interval={1000}>
+                <img style={{
+                  borderRadius: '15px'
+                }}
+                  className="d-block w-100"
+                  src="\img\pusan.jpg"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3 style={{
+                    fontSize: '20px'
+                  }}>부산 해운대</h3>
+                  <h6>해운대 앞바다와 마천루가 선사하는 근사한 야경</h6>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+                <img style={{
+                  borderRadius: '15px'
+                }}
+                  className="d-block w-100"
+                  src="\img\bookchon.jpg"
+                  alt="Second slide"
+                />
+                <Carousel.Caption>
+                  <h3 style={{
+                    fontSize: '20px'
+                  }}>서울 북촌 한옥마을</h3>
+                  <h6>고즈넉한 분위기의 전통 한옥마을</h6>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img style={{
+                  borderRadius: '15px'
+                }}
+                  className="d-block w-100"
+                  src="\img\geoje.jpg"
+                  alt="Third slide"
+                />
+                <Carousel.Caption>
+                  <h3 style={{
+                    fontSize: '20px'
+                  }}>경남 거제시</h3>
+                  <h6>조용하고 따뜻한 바다마을을 여행하고 싶다면.. 거제로!</h6>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
 
-        {/* <Card review={review} /> */}
+          {/* <Card review={review} /> */}
+        </div>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default MainPage;
--- practice_server/index.js
+++ practice_server/index.js
@@ -4,92 +4,86 @@ require('dotenv').config();
 let moment = require('moment');
 let upbit = require("node-upbit");
 
+const cors = require('cors')
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
 
 //세션설정
 let session = require('express-session');
 app.use(
     session({
-        secret : process.env.session,
-        resave : false,
-        saveUninitialized : true,
-        maxAge : 3600000
+        secret: process.env.session,
+        resave: false,
+        saveUninitialized: true,
+        maxAge: 3600000
     })
 )
 
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 
 //mysql 설정
 let mysql = require('mysql2');
 let connection = mysql.createConnection({
-    host : process.env.host,
-    port : process.env.post,
-    user : process.env.user,
-    password : process.env.password,
-    database : process.env.database
+    host: process.env.host,
+    port: process.env.post,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
 })
 
-//up-bit 코인 시세 정보 불러오기
-let quoation = upbit.QuoationService;
-const service = new quoation();
-let quoations = new Array;
 
-async function getQuoation(){
-    let btc = await service.getTicker(["KRW-BTC"]);
-    let eth = await service.getTicker(["KRW-ETH"]);
-    let xrp = await service.getTicker(["KRW-XRP"]);
-    let neo = await service.getTicker(["KRW-neo"]);
+let port = 3010;
+app.listen(port, function(){
+    console.log('express 서버를 시작합니다');
+})
 
-    quoations[0] = btc[0]['trade_price'];
-    quoations[1] = btc[0]['signed_change_price'];
-    quoations[2] = eth[0]['trade_price'];
-    quoations[3] = eth[0]['signed_change_price'];
-    quoations[4] = xrp[0]['trade_price'];
-    quoations[5] = xrp[0]['signed_change_price'];
-    quoations[6] = neo[0]['trade_price'];
-    quoations[7] = neo[0]['signed_change_price'];
 
-    console.log(quoations);
 
-    connection.query(
-        `update quoation set trade_price = ?, signed_change_price = ? where market = 'btc'`, [quoations[0], quoations[1]],
-        function(err){
-            if(err){
-                console.log('btc 시세 변경 에러: '+err)
-            }else{
-                console.log('btc 시세변경 성공')
-            }
-        }
-    );
-    connection.query(
-        `update quoation set trade_price = ?, signed_change_price = ? where market = 'eth'`, [quoations[2], quoations[3]],
-        function(err){
-            if(err){
-                console.log('eth 시세 변경 에러: '+err)
-            }else{
-                console.log('eth 시세변경 성공')
-            }
-        }
-    );
-    connection.query(
-        `update quoation set trade_price = ?, signed_change_price = ? where market = 'xrp'`, [quoations[4], quoations[5]],
-        function(err){
-            if(err){
-                console.log('xrp 시세 변경 에러: '+err)
-            }else{
-                console.log('xrp 시세변경 성공')
-            }
-        }
-    );
-    connection.query(
-        `update quoation set trade_price = ?, signed_change_price = ? where market = 'neo'`, [quoations[6], quoations[7]],
-        function(err){
-            if(err){
-                console.log('neo 시세 변경 에러: '+err)
-            }else{
-                console.log('neo 시세변경 성공')
-            }
-        }
-    );
-}
-setInterval(getQuoation,10000);
+
+
+
+
+    // connection.query(
+    //     `update quoation set trade_price = ?, signed_change_price = ? where market = 'btc'`, [quoations[0], quoations[1]],
+    //     function (err) {
+    //         if (err) {
+    //             console.log('btc 시세 변경 에러: ' + err)
+    //         } else {
+    //             console.log('btc 시세변경 성공')
+    //         }
+    //     }
+    // );
+    // connection.query(
+    //     `update quoation set trade_price = ?, signed_change_price = ? where market = 'eth'`, [quoations[2], quoations[3]],
+    //     function (err) {
+    //         if (err) {
+    //             console.log('eth 시세 변경 에러: ' + err)
+    //         } else {
+    //             console.log('eth 시세변경 성공')
+    //         }
+    //     }
+    // );
+    // connection.query(
+    //     `update quoation set trade_price = ?, signed_change_price = ? where market = 'xrp'`, [quoations[4], quoations[5]],
+    //     function (err) {
+    //         if (err) {
+    //             console.log('xrp 시세 변경 에러: ' + err)
+    //         } else {
+    //             console.log('xrp 시세변경 성공')
+    //         }
+    //     }
+    // );
+    // connection.query(
+    //     `update quoation set trade_price = ?, signed_change_price = ? where market = 'neo'`, [quoations[6], quoations[7]],
+    //     function (err) {
+    //         if (err) {
+    //             console.log('neo 시세 변경 에러: ' + err)
+    //         } else {
+    //             console.log('neo 시세변경 성공')
+    //         }
+    //     }
+    // );
+
+
+// setInterval(getQuoation, 10000);
